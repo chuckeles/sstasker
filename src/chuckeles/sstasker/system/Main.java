e5ca29c4589fadc3598b263edb2c7568195e16cd
Martin Chuckeles <martin@jakubik.info>
2015-04-01 13:14:30 +0200

    Added Window.fxml and set it up in the application

--- src/chuckeles/sstasker/system/Main.java
+++ src/chuckeles/sstasker/system/Main.java
@@ -2,6 +2,8 @@ package chuckeles.sstasker.system;
 
 /**
  * Created by Martin on 01.04.2015.
+ *
+ * This class contains the main method.
  */
 public class Main {
 
@@ -14,7 +16,7 @@ public class Main {
 
     Log.Instance().Log("SSTasker is starting");
 
-    SSTasker.launch(args);
+    SSTasker.launch(SSTasker.class, args);
 
     Log.Instance().Log("SSTasker is ending");
 
--- src/chuckeles/sstasker/system/SSTasker.java
+++ src/chuckeles/sstasker/system/SSTasker.java
@@ -1,20 +1,58 @@
 package chuckeles.sstasker.system;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by Martin on 01.04.2015.
+ *
+ * The SSTasker Application.
  */
 public class SSTasker extends Application {
 
+  /**
+   * Start the SSTasker application.
+   *
+   * @param primaryStage Main stage
+   */
   @Override
   public void start(Stage primaryStage) {
+
+    // set up main stage
     mPrimaryStage = primaryStage;
+    mPrimaryStage.setTitle("SSTasker");
+    mPrimaryStage.setMinWidth(400);
+    mPrimaryStage.setMinHeight(300);
+
+    // load the root layout
+    try {
+      FXMLLoader loader = new FXMLLoader(SSTasker.class.getResource("/chuckeles/sstasker/view/Window.fxml"));
+      mWindow = (BorderPane)loader.load();
+    }
+    catch (IOException e) {
+      Log.Instance().Log(e.getMessage());
+    }
+
+    // show the scene
+    mPrimaryStage.setScene(new Scene(mWindow));
+    mPrimaryStage.show();
+
   }
 
   // ------
 
+  /**
+   * Application's main stage.
+   */
   private Stage mPrimaryStage;
+  /**
+   * Main border pane which is the window for the application.
+   */
+  private BorderPane mWindow;
 
 }
