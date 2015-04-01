ceacad1db28b6393f39554863e5990714c4ddca0
Martin Chuckeles <martin@jakubik.info>
2015-04-01 14:04:12 +0200

    Copied Task

---
+++ src/chuckeles/sstasker/model/task/Task.java
@@ -0,0 +1,75 @@
+package chuckeles.sstasker.model.task;
+
+/**
+ * Created by Martin on 20.03.2015.
+ *
+ * A task with a name and a description. A task can have assigned crew members and inventory items.
+ */
+public class Task {
+
+  /**
+   * Create a task with default title.
+   */
+  public Task() {}
+
+  /**
+   * Create a task with title.
+   *
+   * @param title Task title
+   */
+  public Task(String title) {
+    this();
+    mTitle = title;
+  }
+
+  // ---
+
+  /**
+   * Set task's description.
+   *
+   * @param description New description
+   */
+  public void SetDescription(String description) {
+    mDescription = description;
+  }
+
+  /**
+   * Set task's title.
+   *
+   * @param title New title
+   */
+  public void SetTitle(String title) {
+    mTitle = title;
+  }
+
+  /**
+   * Get task's description.
+   *
+   * @return Task's description
+   */
+  public String GetDescription() {
+    return mDescription;
+  }
+
+  /**
+   * Get task's title.
+   *
+   * @return Task's title
+   */
+  public String GetTitle() {
+    return mTitle;
+  }
+
+  // ---
+
+  /**
+   * The title of the task.
+   */
+  String mTitle = "New Task";
+
+  /**
+   * The description of the task.
+   */
+  String mDescription = "";
+
+}
