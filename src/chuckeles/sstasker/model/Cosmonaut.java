5cde69ce4f3ebb0cc0e3311d090166ae0ca6b7a6
Martin Chuckeles <martin@jakubik.info>
2015-04-08 14:04:59 +0200

    Added task list to the Spaceship

---
+++ src/chuckeles/sstasker/model/Cosmonaut.java
@@ -0,0 +1,7 @@
+package chuckeles.sstasker.model;
+
+/**
+ * Created by Martin on 8.4.2015.
+ */
+public class Cosmonaut {
+}
--- src/chuckeles/sstasker/model/Spaceship.java
+++ src/chuckeles/sstasker/model/Spaceship.java
@@ -1,5 +1,8 @@
 package chuckeles.sstasker.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The main model of our spaceship. Singleton. Contains the list of tasks, crew members and inventory items.
  */
@@ -21,10 +24,24 @@ public class Spaceship {
     return mInstance;
   }
 
+  /**
+   * Get the list of tasks.
+   *
+   * @return The list of tasks
+   */
+  public List<Task> GetTasks() {
+    return mTasks;
+  }
+
   // ------
 
   /**
    * The singleton instance.
    */
   private static Spaceship mInstance;
+
+  /**
+   * The list of tasks.
+   */
+  private List<Task> mTasks = new ArrayList<>();
 }
