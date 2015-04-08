099596650c41dfaab4e00c78ac1bb59b4fded627
Martin Chuckeles <martin@jakubik.info>
2015-04-08 14:05:08 +0200

    Added basic cosmonaut class

--- src/chuckeles/sstasker/model/Cosmonaut.java
+++ src/chuckeles/sstasker/model/Cosmonaut.java
@@ -1,7 +1,35 @@
 package chuckeles.sstasker.model;
 
 /**
- * Created by Martin on 8.4.2015.
+ * Contains information about a crew member.
  */
+// TODO: Mark abstract
 public class Cosmonaut {
+
+  /**
+   * Create new cosmonaut.
+   *
+   * @param name The name of the cosmonaut
+   */
+  public Cosmonaut(String name) {
+    mName = name;
+  }
+
+  // ------
+
+  /**
+   * Get the name of the cosmonaut.
+   *
+   * @return The name of the cosmonaut
+   */
+  public String GetName() {
+    return mName;
+  }
+
+  // ------
+
+  /**
+   * Name of the cosmonaut.
+   */
+  private String mName;
 }
