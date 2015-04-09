7374e7468e16f2f242361aeec1c6d2ccec05756c
Martin Chuckeles <martin@jakubik.info>
2015-04-09 21:12:26 +0200

    Added animation between states

--- src/chuckeles/sstasker/view/js/main.js
+++ src/chuckeles/sstasker/view/js/main.js
@@ -1,10 +1,10 @@
-angular.module("sstasker", ["ui.router"])
+angular.module("sstasker", ["ngAnimate", "ui.router"])
 
   .controller("mainCtrl", ["$state", function($state) {
     console.log("AngularJS loaded, going to the welcome state");
 
     // go to welcome state
-    $state.go("dashboard"); // TODO: Change back to welcome
+    $state.go("welcome"); // TODO: Change back to welcome
   }]);
 
 //@prepros-append constants.js
