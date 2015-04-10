41bada6c7791140d7d49e02257ad7f687f956dc8
Martin Chuckeles <martin@jakubik.info>
2015-04-10 09:36:36 +0200

    Added javadoc for JSInterface functions

--- src/chuckeles/sstasker/system/JSInterface.java
+++ src/chuckeles/sstasker/system/JSInterface.java
@@ -5,10 +5,18 @@ package chuckeles.sstasker.system;
  */
 public class JSInterface {
 
+  /**
+   * Log the text.
+   *
+   * @param text Text to load
+   */
   public void log(String text) {
     Log.Instance().Log("[webview] " + text);
   }
 
+  /**
+   * Exit the application.
+   */
   public void exit() {
     System.exit(0); // TODO: Handle more intelligently
   }
